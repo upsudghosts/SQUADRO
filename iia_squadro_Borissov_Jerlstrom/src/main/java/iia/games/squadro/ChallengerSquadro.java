a92795ef6e49a49f1a29cbe5f01ac3224bb4eece
augej <augej@DESKTOP-NJD7GP7>
2020-04-26 15:53:25 +0200

    on essaye de reparer cette daube

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/ChallengerSquadro.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/ChallengerSquadro.java
@@ -2,10 +2,57 @@ package iia.games.squadro;
 
 import iia.games.base.IChallenger;
 
-public class ChallengerSquadro implements IChallenger<MoveSquadro> {
+public class ChallengerSquadro implements IChallenger {
 
-    @Override
     public MoveSquadro pickMove(MoveSquadro enemyMove) {
         return null;
     }
+
+	@Override
+	public String teamName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRole(String role) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iPlay(String move) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void otherPlay(String move) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String bestMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String victory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String defeat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String tie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
