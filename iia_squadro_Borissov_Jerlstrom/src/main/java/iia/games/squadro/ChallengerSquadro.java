f405cf57de2b3d09bc6b3aef957e978148e5b678
augej <augej@DESKTOP-NJD7GP7>
2020-05-01 18:27:03 +0200

    asf

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/ChallengerSquadro.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/ChallengerSquadro.java
@@ -81,13 +81,13 @@ public class ChallengerSquadro implements IChallenger {
 		String m1, m2;
 		String moveF = "";
 		
-		System.out.println("AVANT:" + board.Board.get(piece).x);
+		System.out.println("AVANT:" + board.Board.get(piece).y);
 		Point avant = board.Board.get(piece);
 		
 		BoardSquadro boardCopy = board.copy();
 		
 		boardCopy = boardCopy.play(move, joueur);
-		System.out.println("APRES:" + boardCopy.Board.get(piece).x);
+		System.out.println("APRES:" + boardCopy.Board.get(piece).y);
 		Point apres = boardCopy.Board.get(piece);
 		
 		switch(joueur){
