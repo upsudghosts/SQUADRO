699ef866d4f5720b482b53a29ab69e39c0e59b78
augej <augej@DESKTOP-NJD7GP7>
2020-05-03 01:06:58 +0200

    asdf

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
@@ -83,14 +83,14 @@ public class HeuristicSquadro {
 			break;
 		}
 	
-		/*
+		
 		if(nbAmiFini == 4) {
 			scoreAmi += 400;
 		}
 		if(nbAdvFini == 4) {
 			scoreAdv += 400;
 		}
-		*/
+		
 
 		int heuristicValue = scoreAmi  - scoreAdv;
 		return heuristicValue;
