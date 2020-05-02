bc96c8696cae1d64f612cdc4c4d9f6268e910e5d
augej <augej@DESKTOP-NJD7GP7>
2020-05-02 21:39:22 +0200

    asdf

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/algo/AlphaBeta.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/algo/AlphaBeta.java
@@ -13,7 +13,7 @@ public class AlphaBeta<M extends IMove, R extends IRole, B extends IBoard<M, R, 
 	/**
 	 * La profondeur de recherche par défaut
 	 */
-	private final static int PROFMAXDEFAUT = 4;
+	private final static int PROFMAXDEFAUT = 1;
 
 	// -------------------------------------------
 	// Attributs
