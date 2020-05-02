e7a394f54997ec0371a4c7fe82f8ea6db1f57b35
augej <augej@DESKTOP-NJD7GP7>
2020-05-02 23:07:50 +0200

    new heuristique

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
@@ -6,89 +6,53 @@ public class HeuristicSquadro {
 	public static final IHeuristic<RoleSquadro, BoardSquadro> hZero = (BoardSquadro, r) -> 0;
 
 	public static final IHeuristic<RoleSquadro, BoardSquadro> hReal = (BoardSquadro, r) -> {
-		int nbPionsFini = 0;
-		int nbPionsAlignesAmi = 0;
-		int nbPionsAdvDanger = 0;
-		int speedAdv = 0;
+		int scoreAmi = 0;
+		int scoreAdv = 0;
 		BoardSquadro bCop = BoardSquadro.copy();
-
-		switch (r) {
+		switch(r) {
 		case VERTICAL:
-
-			for (int i = 5; i < 10; i++) {
-				// calculs pour tout ce qui est AMI
-				if (bCop.Board.get(i).y == 1 && bCop.Board.get(i).x == 0)
-					nbPionsFini++;
-				// Nb pions ami alignes
-				if (i < 9) {
-					if (bCop.Board.get(i).x == bCop.Board.get(i + 1).x)
-						nbPionsAlignesAmi++;
-				}
-
-				// ALLER
-				int vitessePion = bCop.vitesse.get(i).x;
-				for (int j = 0; j <= vitessePion; j++) {
-
-					if (bCop.Board.get(i).y == 0) {
-
-						int advCoord = 4 - bCop.Board.get(i).x - j;
-						if (advCoord >= 0) {
-
-							if (i - 4 == bCop.Board.get(advCoord).x)
-								nbPionsAdvDanger++;// regarde si on se trouve pile devant un pion adverse
-						}
-						// RETOUR
-					} else if (bCop.Board.get(i).y == 1 && bCop.Board.get(i).x != 0) {
-
-						int advCoord = 6 - bCop.Board.get(i).x + j;
-						if (advCoord < 5) {
-							if (i - 4 == bCop.Board.get(advCoord).x)
-								nbPionsAdvDanger++;// regarde si on se trouve pile devant un pion adverse
-						}
+			
+			for(int i = 0; i < 10; i++) {
+				if(i <=4) {//ADV
+					if(bCop.Board.get(i).y==1) {
+						scoreAdv += 6 + (6 - bCop.Board.get(i).x);
+					}else {
+						scoreAdv += bCop.Board.get(i).x;
+					}
+				} else {//AMI
+					if(bCop.Board.get(i).y==1) {
+						scoreAmi += 6 + (6 - bCop.Board.get(i).x);
+					}else {
+						scoreAmi += bCop.Board.get(i).x;
 					}
 				}
-
 			}
-
+			
 			break;
-
 		case HORIZONTAL:
-
-			for (int i = 0; i < 5; i++) {
-				// calculs pour tout ce qui est AMI
-				if (bCop.Board.get(i).y == 1 && bCop.Board.get(i).x == 0)
-					nbPionsFini++;
-				// Nb pions ami alignes
-				if (i < 4) {
-					if (bCop.Board.get(i).x == bCop.Board.get(i + 1).x)
-						nbPionsAlignesAmi++;
-				}
-
-				int vitessePion = bCop.vitesse.get(i).x;
-				for (int j = 0; j <= vitessePion; j++) {
-					// ALLER
-					if (bCop.Board.get(i).y == 0) {
-						int advCoord = 5 + bCop.Board.get(i).x + j;
-						if (advCoord <= 9) {
-							if (5 - i == bCop.Board.get(advCoord).x)
-								nbPionsAdvDanger++;
-						}
-						// RETOUR
-					} else if (bCop.Board.get(i).y == 1 && bCop.Board.get(i).x != 0) {
-						int advCoord = 3 + bCop.Board.get(i).x - j;
-						if (advCoord > 4) {
-							if (5 - i == bCop.Board.get(advCoord).x)
-								nbPionsAdvDanger++;
-						}
+			
+			for(int i = 0; i < 10; i++) {
+				if(i <=4) {//AMI
+					if(bCop.Board.get(i).y==1) {
+						scoreAmi += 6 + (6 - bCop.Board.get(i).x);
+					}else {
+						scoreAmi += bCop.Board.get(i).x;
 					}
-
+				} else {//ADV
+					if(bCop.Board.get(i).y==1) {
+						scoreAdv += 6 + (6 - bCop.Board.get(i).x);
+					}else {
+						scoreAdv += bCop.Board.get(i).x;
+					}
 				}
 			}
-
+			
 			break;
 		}
+	
+		
 
-		int heuristicValue = nbPionsAdvDanger * 10;
+		int heuristicValue = scoreAmi - scoreAdv;
 		System.out.println("HEURISTIC VALUE = " + heuristicValue);
 		return heuristicValue;
 	};
