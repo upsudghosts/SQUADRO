188e28f9dc8f848e9354ecaf91f91ac7aa31eb17
augej <augej@DESKTOP-NJD7GP7>
2020-05-02 15:30:51 +0200

    aoisgdigaus

--- iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
+++ iia_squadro_Borissov_Jerlstrom/src/main/java/iia/games/squadro/HeuristicSquadro.java
@@ -4,4 +4,10 @@ import iia.games.base.IHeuristic;
 
 public class HeuristicSquadro  {
 	public static final IHeuristic<RoleSquadro, BoardSquadro> hZero = (boardNim, r) -> 0;
+	
+	public static final IHeuristic<RoleSquadro, BoardSquadro> hReal = (boardNim, r) -> {
+		
+		
+		return 1;
+	};
 }
