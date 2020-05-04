9ea514e166e76f3bcda25d7a4d59fe6c205f6c86
augej <augej@DESKTOP-NJD7GP7>
2020-05-04 17:13:10 +0200

    asdf

--- iia_squadro_Borissov_Jerlstrom/launch.sh
+++ iia_squadro_Borissov_Jerlstrom/launch.sh
@@ -9,7 +9,7 @@ xterm -rightbar -fa dejavumono -fs 12 -title  "Server" -geometry 90x26 -fg grey 
 
 sleep 0.5
 
-xterm -rightbar -fa dejavumono -fs 12 -title  "Random" -geometry 90x26 -fg grey -bg black -e  "java -cp SquadroINAB.jar:commons-cli-1.4.jar iia.games.contest.Client -p 4536 -s localhost -c iia.games.squadro.ChallengerSquadro;bash" &
+xterm -rightbar -fa dejavumono -fs 12 -title  "Random" -geometry 90x26 -fg grey -bg black -e  "java -cp iia-referee.jar iia.games.contest.Client -p 4536 -s localhost -c iia.games.squadro.ChallengerSquadro;bash" &
 
 xterm -rightbar -fa dejavumono -fs 12 -title  "Nous" -geometry 90x26 -fg grey -bg black -e  "java -cp iia_BORISSOV_JERLSTROM.jar:commons-cli-1.4.jar iia.games.contest.Client -p 4536 -s localhost -c iia.games.squadro.ChallengerSquadro;bash" &
 
